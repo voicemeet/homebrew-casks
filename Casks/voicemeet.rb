6756988526171a7e79dd277efaf6e68afa909728
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2026-03-30 01:33:05 +0000

    Update voicemeet cask to v0.0.5
    Signed-off-by: kjxbyz <47768002+kjxbyz@users.noreply.github.com>

--- Casks/voicemeet.rb
+++ Casks/voicemeet.rb
@@ -1,5 +1,5 @@
 cask "voicemeet" do
-  version "0.0.4"
+  version "0.0.5"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/voicemeet/releases/releases/download/v#{version}/VoiceMeet-#{version.tr("+", ".")}-universal.dmg",
